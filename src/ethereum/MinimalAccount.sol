9c877c2c7a1c73e558a342ae90b808342d8a7f83
zntb <zenetibi@yahoo.com>
2024-11-26 08:19:45 +0200

    Ethereum Setup

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount is IAccount {
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {}
+}
